b213df1e451800e033aae3c47d9e10d3fb46e4e8
TomasAldea <tomas.sbd.5@gmail.com>
2021-03-07 19:05:50 +0100

    recipe-crud-create-delete-done

--- routes/recipe.routes.js
+++ routes/recipe.routes.js
@@ -3,12 +3,16 @@ const route = Router();
 
 const {
     
-recipeCreate
+recipeCreate,
+recipeDelete,
+recipeUpdate,
 
 } = require("../controllers/recipe.controllers.js")
 
 route
 .post("/recipe", recipeCreate)
+.put("recipe/:recipeId", recipeUpdate)
+.delete("/recipe/:recipeId", recipeDelete)
 
 
 module.exports = route;
