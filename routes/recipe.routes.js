69cb40fddaeb9da09c60711a6d8c01014e1d69c4
TomasAldea <tomas.sbd.5@gmail.com>
2021-03-08 20:52:34 +0100

    get all recipes done, week model testing

--- routes/recipe.routes.js
+++ routes/recipe.routes.js
@@ -7,11 +7,13 @@ recipeCreate,
 recipeDelete,
 recipeUpdate,
 getRecipe,
+getRecipes,
 
 } = require("../controllers/recipe.controllers.js")
 
 route
 .get("/recipe/:recipeId", getRecipe)
+.get("/allrecipes" , getRecipes)
 .post("/recipe", recipeCreate)
 .post("recipe/:recipeId", recipeUpdate)
 .delete("/recipe/:recipeId", recipeDelete)
