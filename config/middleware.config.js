930c09835914bef7f2b064f34cf2c7b77869b4c0
TomasAldea <tomas_aldea@DESKTOP-58B551K.localdomain>
2021-03-18 20:29:55 +0100

    cors testings

--- config/middleware.config.js
+++ config/middleware.config.js
@@ -1,13 +1,23 @@
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
-const origin = "http://localhost:3000";
-const corsConfig = { origin: [origin], credentials: true };
+const origin = process.env.PUBLIC_DOMAIN;
+//const corsConfig = { origin: [origin], credentials: true };
 
 module.exports = (app) => {
-  app.use(cors(corsConfig));
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  }); 
+  //app.use(cors(corsConfig));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(cookieParser());
 };
+
+
